dff37bb3817f32d3059715bb1f1ab45361f5843e
vladhusasan <84732563+vladhusasan@users.noreply.github.com>
2021-11-14 13:19:09 +0200

    Add files via upload

---
+++ Husasan_Vlad/Tema5_DATC/Tema5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tema5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
